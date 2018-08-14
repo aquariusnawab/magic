5c5287ffc43b050ffd3834c55eb5d1fa921aeefd
aquariusnawab <42366965+aquariusnawab@users.noreply.github.com>
2018-08-14 10:26:52 +0000

    Naseer

--- newGit/src/package1/verifyGit.java
+++ newGit/src/package1/verifyGit.java
@@ -5,7 +5,7 @@ public class verifyGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Git Obtaned");
-		System.out.println("Git retrived");
+		System.out.println("Git retrieved Naseer");
 	}
 
 }
